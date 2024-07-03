930ce0a44087482bfe01e89bb06b124e06b69ad3
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-07-03 11:28:25 +0300

    Your commit message

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,4 @@ def perform_operation(num1, num2, operation):
         if num2 != 0:
             return num1 / num2
         else:
-            return "Error: Division by zero."
-    else:
-        return "Error: Unsupported operation."
+            return "cannot divide by zero "
