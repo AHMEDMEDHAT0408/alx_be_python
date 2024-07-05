fe717d23fa1b05eaa70c5696eb50b57b17da9274
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-07-05 12:34:31 +0300

    added the arithmetic_operations.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
+        if num2 == 0:
+            return "cannot divide by zero"
         else:
-            return "cannot divide by zero "
+            return num1 / num2
