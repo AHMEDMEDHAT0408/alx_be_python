5a5d76503f8683bbfcc15377aa2949a3301567f3
Ahmed Medhat <149432254+AHMEDMEDHAT0408@users.noreply.github.com>
2024-07-27 02:32:23 +0300

    added the files

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,6 @@
-class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+class Book:
+    def __init__(self,title,author,year):
+        self.title = title
+        self.author = author
+        self.year = year
 
-    def deposit(self, amount):
-        self.account_balance += amount
-
-    def withdraw(self, amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
-            return True
-        return False
-
-    def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
